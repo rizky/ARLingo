0b123b3c3e9b5e320b6b0950c64e10bbb4608d2b
rizkyario <rizkyario@yahoo.com>
2018-04-09 21:00:31 +0200

    Added prebuild

---
+++ prebuild.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+$APPCENTER_SOURCE_DIRECTORY/PlacingObjects/Resources/VGG16.mlmodelc/model.espresso.weights
+
+wget http://51.144.52.98/s/NkQcDLKb6CBgL6E/download -P $APPCENTER_SOURCE_DIRECTORY/PlacingObjects/Resources/VGG16.mlmodelc/model.espresso.weights
